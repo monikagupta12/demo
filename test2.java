cfa90a98f5fce7e44d4a759b44eb365380dc137a
Monika <monikagupta120891@gmail.com>
2024-10-22 03:44:09 +0530

    leftbrabch push

--- test2.java
+++ test2.java
@@ -1,10 +1,11 @@
 public class test2 {
     public static void main(String[] args) {
 
-        System.out.println("do something");
+        System.out.println("do something")
         System.out.println("add Somthing");
         System.out.println("left branch add");
         System.out.println("confit change");
+        System.out.println("check int leftbranch push");
 
     }
 }
