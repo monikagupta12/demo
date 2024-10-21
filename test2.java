d966f12eeb23639a55444f5d7e96b946e6a6f482
Monika <monikagupta120891@gmail.com>
2024-10-22 00:22:09 +0530

    check diff before add

--- test2.java
+++ test2.java
@@ -1,5 +1,8 @@
 public class test2 {
     public static void main(String[] args) {
+
         System.out.println("do something");
+        System.out.println("add Somthing");
+        System.out.println("new somthing");
     }
 }
