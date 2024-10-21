ad0f88bc733bb711e4b97945909f52c96b00512c
Monika <monikagupta120891@gmail.com>
2024-10-21 23:02:04 +0530

    next line

--- test1.java
+++ test1.java
@@ -1,5 +1,7 @@
 public class test1 {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("new line");
+        System.out.println("next line");
     }
 }
